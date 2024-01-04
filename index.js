409df4b9ba158723b939770ad93ae6da8449efb7
Rinad Abu Qauod <rinadaq98@gmail.com>
2024-01-04 13:26:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+// helpers.js
+
+function superbowlWin(record) {
+    const winYear = record.find(entry => entry.result === "W");
+  
+    return winYear ? winYear.year : undefined;
+  }
+  
